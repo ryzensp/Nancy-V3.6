01c9a65a18ec4098b4eeac22bec9dba64295c693
Hacker Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2022-04-08 23:48:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/Hacker-Jr-TG/Nancy-V3.5 /Nancy-V3.5
+git clone -b master https://github.com/Hacker-Jr-TG/Nancy-V3.5.git /Nancy-V3.5
 cd /Nancy-V3.5
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
