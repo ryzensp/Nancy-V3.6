6fd9b629011c4582f0066ae3b9a2442cfed9c082
Hacker Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2022-04-09 15:13:10 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -522,8 +522,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.message.send_sticker(
             sticker="CAACAgUAAxkBAAEGCmBiUUy3xeeJqrl3q6OIcAiYvnSzegACGQYAAje7iFZisi8qLpE8XB4E",
             chat_id=query.message.chat.id,
-            reply_markup=reply_markup,
-            parse_mode='html'
+            reply_markup=reply_markup
+            #parse_mode='html'
         )
     elif query.data == "about":
         buttons = [[
