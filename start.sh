ae62953139de7b96b35a078cfe9e204660be6c5b
Hacker Jr <70173542+HackerX7@users.noreply.github.com>
2022-04-11 16:40:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/Hacker-Jr-TG/Nancy-V3.5.git /Nancy-V3.5
-cd /Nancy-V3.5
+git clone -b master https://github.com/HackerX7/Nancy-V3.6.git /Nancy-V3.6
+cd /Nancy-V3.6
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
